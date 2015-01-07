4de4daee8158d24a7dffa3299ce23f41bbb19166
zhouyanming <zhouyanming@gmail.com>
2015-01-07 18:08:34 +0800

    x

--- src/org/ironrhino/wechat/model/WechatMenu.java
+++ src/org/ironrhino/wechat/model/WechatMenu.java
@@ -177,7 +177,7 @@ public class WechatMenu implements Serializable {
 	}
 
 	public static enum WechatButtonType {
-		click, view
+		click, view, scancode_push, scancode_waitmsg, pic_sysphoto, pic_photo_or_album, pic_weixin, location_select
 	}
 
 }
