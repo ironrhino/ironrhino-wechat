313ed67022b40355ce7ed89db565ce5f5dd62294
zhouyanming <zhouyanming@gmail.com>
2015-01-08 16:37:39 +0800

    x

--- src/org/ironrhino/wechat/handler/EchoClickEventHandler.java
+++ src/org/ironrhino/wechat/handler/EchoClickEventHandler.java
@@ -1,10 +1,13 @@
 package org.ironrhino.wechat.handler;
 
+import org.ironrhino.core.spring.configuration.StageConditional;
+import org.ironrhino.core.util.AppInfo.Stage;
 import org.ironrhino.wechat.model.WechatRequest;
 import org.ironrhino.wechat.model.WechatResponse;
 import org.springframework.stereotype.Component;
 
 @Component
+@StageConditional(Stage.DEVELOPMENT)
 public class EchoClickEventHandler extends ClickEventHandler {
 
 	@Override
