0dc66ae3c113f9201f4f87e6f6d7cbe66cd29630
Yanming Zhou <zhouyanming@gmail.com>
2016-12-25 20:44:36 +0800

    Add new button type

--- src/org/ironrhino/wechat/model/WechatMenu.java
+++ src/org/ironrhino/wechat/model/WechatMenu.java
@@ -179,7 +179,7 @@ public class WechatMenu implements Serializable {
 	}
 
 	public static enum WechatButtonType {
-		click, view, scancode_push, scancode_waitmsg, pic_sysphoto, pic_photo_or_album, pic_weixin, location_select
+		click, view, scancode_push, scancode_waitmsg, pic_sysphoto, pic_photo_or_album, pic_weixin, location_select, media_id, view_limited
 	}
 
 }
